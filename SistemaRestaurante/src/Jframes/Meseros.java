e2c6413778df43aaf9448d777fdcec9f6b25705e
ELDURANPAI <110361013+ELDURANPAI@users.noreply.github.com>
2025-04-10 11:02:36 -0600

    cambios

--- SistemaRestaurante/src/Jframes/Meseros.java
+++ SistemaRestaurante/src/Jframes/Meseros.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 package Jframes;
 
 import java.sql.*;
@@ -9,10 +5,6 @@ import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 import sistemarestaurante.*;
 
-/**
- *
- * @author DURAN
- */
 public class Meseros extends javax.swing.JFrame {
 
     public void ConsultarMeseros() {
--- SistemaRestaurante/src/sistemarestaurante/Cocina.java
+++ SistemaRestaurante/src/sistemarestaurante/Cocina.java
@@ -2,16 +2,8 @@ package sistemarestaurante;
 
 import javax.swing.JOptionPane;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Harry Orozco
- */
 public class Cocina {
+
     private String[] vasos = new String[10];
     private String[] platillos = new String[10];
     private int contadorVasos = 0;
@@ -48,26 +40,27 @@ public class Cocina {
     public void setContadorPlatillos(int contadorPlatillos) {
         this.contadorPlatillos = contadorPlatillos;
     }
-    
- public void agregarVaso() {
-    try {
-        String input = JOptionPane.showInputDialog("Ingresa un número de vaso:");
-        if (input != null && !input.equals("")) {
-            int vaso = Integer.parseInt(input);
-            if (contadorVasos < vasos.length) {
-                vasos[contadorVasos] = String.valueOf(vaso);
-                contadorVasos++;
-                JOptionPane.showMessageDialog(null, "Vaso agregado correctamente: " + vaso);
+
+    public void agregarVaso() {
+        try {
+            String input = JOptionPane.showInputDialog("Ingresa un número de vaso:");
+            if (input != null && !input.equals("")) {
+                int vaso = Integer.parseInt(input);
+                if (contadorVasos < vasos.length) {
+                    vasos[contadorVasos] = String.valueOf(vaso);
+                    contadorVasos++;
+                    JOptionPane.showMessageDialog(null, "Vaso agregado correctamente: " + vaso);
+                } else {
+                    JOptionPane.showMessageDialog(null, "No hay espacio para vasos.");
+                }
             } else {
-                JOptionPane.showMessageDialog(null, "No hay espacio para vasos.");
+                JOptionPane.showMessageDialog(null, "No se puede agregar un vaso.");
             }
-        } else {
-            JOptionPane.showMessageDialog(null, "No se puede agregar un vaso.");
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "ingrese un número valido.");
         }
-    } catch (NumberFormatException e) {
-        JOptionPane.showMessageDialog(null, "ingrese un número valido.");
     }
- }
+
     public void agregarPlatillo() {
         String platillo = JOptionPane.showInputDialog("Ingresa el nombre del platillo:");
         if (platillo != null && !platillo.equals("")) {
@@ -89,5 +82,5 @@ public class Cocina {
                 + "\nNumero de vasos: " + vasos
                 + "\nNumero de Platillos: " + platillos;
     }
-    
+
 }
